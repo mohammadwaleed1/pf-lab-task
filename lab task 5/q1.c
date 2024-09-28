622e790ee1fe023755b892b17293ed0a8fe974e9
mohammadwaleed1 <waleedmohammad484@gmail.com>
2024-09-28 00:48:12 -0700

    Delete lab task 5 directory

--- lab task 5/q1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-int age;
-int ct;
-printf("Enter your age: ");
-scanf("%d", &age);
-age >= 18 ? printf("You can vote.\n") : printf("You cannot vote.\n");
-return 0;
-}
