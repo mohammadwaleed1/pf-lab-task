7870d74373e54ed03c2421ae178be7f36dee40c7
mohammadwaleed1 <waleedmohammad484@gmail.com>
2024-09-29 19:26:18 +0500

    Add files via upload

---
+++ lab task 4/q4.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+int main(){
+	int N;
+	printf("enter your number:");
+	scanf("%d", &N);
+		switch (N){
+			case 1:
+				printf("one");
+				break;
+			case 2:
+				printf("two");
+				break;
+			case 3:
+				printf("three");
+				break;
+			case 4:
+				printf("four");
+				break;
+			case 5:
+				printf("five");
+				break;
+			case 6:
+				printf("six");
+				break;
+			case 7:
+				printf("seven");
+				break;
+			case 8:
+				printf("eight");
+				break;
+			case 9:
+				printf("nine");
+				break;
+			case 10:
+				printf("ten");
+				break;
+			case 11:
+				printf("eleven");
+				break;
+			case 12:
+				printf("twelve");
+				break;
+			case 13:
+				printf("thirteen");
+				break;
+			case 14:
+				printf("fourteen");
+				break;
+			case 15:
+				printf("fifteen");
+				break;	
+			case 16:
+				printf("sixteen");
+				break;
+			case 17:
+				printf("seventeen");
+				break;
+			case 18:
+				printf("eighteen");
+				break;
+			case 19:
+				printf("ninteen");
+				break;
+		    case 20:
+				printf("twenty");
+				break;		
+			default:
+				printf("number greater than 20");
+				
+					
+		
+		
+		}
+	
+	
+	
+}
---
+++ lab task 4/q7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	printf("        ____________\n");
+    printf("       /            \\\n");
+    printf("      /  /\\_/\\       \\\n");
+    printf("     /  ( . . )  ^    \\\n");
+    printf("    /    )   (  ((     \\\n");
+    printf("   |    ( > < )_))      |\n");
+    printf("    \\   -------        /\n");
+    printf("     \\  K24-0772      /\n");
+    printf("      \\              /\n");
+    printf("       \\____________/\n");
+    return 0;
+}
+
